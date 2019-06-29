1da00010e53e8867c7c99334bd9060dc9a6f2d93
apokkalyps <32598875+apokkalyps@users.noreply.github.com>
2019-06-29 21:31:58 +0200

    Integral rework of module File

--- C++/File.cpp
+++ C++/File.cpp
@@ -8,143 +8,113 @@
 #include <algorithm>
 #include <locale>
 #include <codecvt>
-#include <sys/stat.h>
 #include "File.h"
-#include "../Globals.h"
 
 using std::string; 
 using std::locale; 
 using std::ifstream; 
 using std::ios_base;
 
-namespace SystemIO
+namespace File
 {
-	namespace File
-	{
 /////////////////////////////////////////////////////////////////  PRIVATE
-		namespace
-		{
+
 //-------------------------------------------------------------- Constants
 
-			// Locale for UTF-16LE encoding.
-			const locale locUTF8;
-			const locale locUTF16LE(
-				locale(),
-				new std::codecvt_utf16<char, 0x10ffff, std::little_endian>
-			); 
+	const locale locUTF8(
+		locale(), 
+		new std::codecvt_utf8<char>());
+	const locale locUTF16LE(
+		locale(),
+		new std::codecvt_utf16<char, 0x10ffff, std::little_endian>
+	); 
+
 //------------------------------------------------------------------ Types
 
 //------------------------------------------------------- Static variables
 
 //------------------------------------------------------ Private functions
-			
-			// Returns true if file exists.
-			// Inputs: any filename.
-			bool exists(const string& filename)
-			{
-				struct stat d;
-				return !stat(filename.c_str(), &d);
-			}
-
-			// Returns false if file is less than 4 chars long.
-			// Inputs: any filename.
-			// Warnings: throws a FileException if file does not exist 
-			// or cannot be read.
-			bool isEmpty(const string& filename)
-			{
-				if (!exists(filename))
-				{
-					throw S_Exception(Err::FILENOEXISTS, filename.c_str());
-				}
-				
-				FILE* f = fopen(filename.c_str(), "r");
-				if (f == nullptr)
-				{
-					throw S_Exception(Err::FILEOPEN, filename.c_str());
-				}
-
-				bool forReturn;
-				fgetc(f);
-				fgetc(f);
-				fgetc(f);
-				fgetc(f);
-				forReturn = feof(f) + ferror(f);
-				fclose(f);
-				return forReturn;
-			}
-		}
 
 //////////////////////////////////////////////////////////////////  PUBLIC
 //------------------------------------------------------- Public functions
 
-		void Open(ifstream& ifs, const string& filename)
-		{
-			ifs.close();
-			string encoding = File::Encoding(filename);
+	bool Exists(const string& filename)
+	{
+		DWORD attr = GetFileAttributes(filename);
+		return attr == INVALID_FILE_ATTRIBUTES || (attr & FILE_ATTRIBUTE_DIRECTORY);
+	}
 
-			if (encoding.compare(ENC_UTF8) == 0)
-			{
-				ifs.open(filename);
-				ifs.imbue(locUTF8);
-				ifs.seekg(3);
-				return;
-			}
-			else if (encoding.compare(ENC_UTF16LE) == 0)
-			{
-				ifs.open(filename, ios_base::binary);
-				ifs.imbue(locUTF16LE);
-				ifs.seekg(2, ios_base::beg);
-				return;
-			}
-			else if (encoding.compare(ENC_DEFAULT) == 0)
-			{
-				ifs.open(filename, ios_base::binary);
-				return;
-			}
-			else {
-				throw S_Exception(Err::UNKNOWN, filename.c_str());
-			}
+	bool IsEmpty(const string& filename, size_t charsToRead)
+	{
+		bool forReturn;
+
+		FILE* f = fopen(filename.c_str(), "r");
+		if (f == nullptr)
+		{
+			forReturn = true;
 		}
-
-		filesize_t Size(const string& filename)
+		else
 		{
-			if (!exists(filename))
-				throw S_Exception(Err::FILENOEXISTS, filename.c_str()); 
-			
-			// Converting file name to wstring (unsafe way but we think that 
-			std::wstring wfilename (filename.cbegin(), filename.cend());
-
-			// Now we use Windows WIN32 API tools.
-			WIN32_FILE_ATTRIBUTE_DATA fileInfo;
-			if (!GetFileAttributesEx(wfilename.c_str(), GetFileExInfoStandard, (void*)& fileInfo))
-				throw S_Exception(Err::FILEOPEN, filename.c_str()); // Failed to get file infos.
-			return fileInfo.nFileSizeLow;
-			
-			/* I decided not to use the usual (below) because 
-			 * it may create a wrong size value on a big file. */
-			
-			/*FILE* f = fopen(filename.c_str(), "r");
-			if (f == nullptr) 
-				throw S_Exception(Err::FILEOPEN, filename.c_str());
-
-			fseek(f, 0, SEEK_END);
-			size_t taille = ftell(f);
+			for (size_t i=0;i<charsToRead;++i)
+			{
+				fgetc(f);
+			}
+			forReturn = feof(f) + ferror(f);
 			fclose(f);
-			return taille;*/
 		}
 
-		string Encoding(const string& filename)
-		{
-			if (isEmpty(filename))
-				throw S_Exception(Err::FILEEMPTY, filename.c_str());
+		return forReturn;
+	}
 
+	void Open(ifstream& ifs, const string& filename)
+	{
+		ifs.close();
+		encoding_t encoding = File::Encoding(filename);
+
+		if (encoding == ENC_UTF8)
+		{
+			ifs.open(filename);
+			ifs.imbue(locUTF8);
+			ifs.seekg(3);
+		}
+		else if (encoding == ENC_UTF16LE)
+		{
+			ifs.open(filename, ios_base::binary);
+			ifs.imbue(locUTF16LE);
+			ifs.seekg(2, ios_base::beg);
+		}
+		else if (encoding == ENC_DEFAULT)
+		{
+			ifs.open(filename, ios_base::binary);
+		}
+	}
+
+	filesize_t Size(const string& filename)
+	{
+		std::wstring wfilename (filename.cbegin(), filename.cend());
+
+		HANDLE file = CreateFile(wfilename, GENERIC_READ, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
+		if (file == INVALID_HANDLE_VALUE) return -1;
+		LARGE_INTEGER res;
+		GetFileSizeEx(file, &res);
+		return res.QuadPart;
+	}
+
+	encoding_t Encoding(const string& filename)
+	{
+		encoding_t forReturn;
+		if (IsEmpty(filename, 3))
+			forReturn = "0";
+		else
+		{
 			FILE* f = fopen(filename.c_str(), "r");
+			fseek(f, 0, SEEK_SET);
 
 			int char1 = fgetc(f);
 			int char2 = fgetc(f);
 			int char3 = fgetc(f);
 
-			string forReturn;
+			
 			if (char1 == 0xff && char2 == 0xfe)
 				forReturn = ENC_UTF16LE;
 			else if (char1 == 0xef && char2 == 0xbb && char3 == 0xbf)
@@ -153,7 +123,7 @@ namespace SystemIO
 				forReturn = ENC_DEFAULT;
 
 			fclose(f);
-			return forReturn;
 		}
+		return forReturn;
 	}
 } 
--- C++/File.h
+++ C++/File.h
@@ -1,3 +1,10 @@
+/* Apokk - MyWorks package - File module.
+ 
+ * This module gives some basic informations about a given file.
+ * 
+
+ */
+
 //---------- Interface of module <File> (file File.h) 
 #if ! defined ( FILE_H )
 #define FILE_H
@@ -6,43 +13,47 @@
 #include <fstream>
 #include <string>
 
-// Role of File:
-//	Encapsulates the opening of a file input stream.
-namespace SystemIO
+#ifndef FILESIZE_T
+#define FILESIZE_T
+// Type used to deal with file sizes of any weight (GBs are okay).
+typedef unsigned long filesize_t
+#endif // FILESIZE_T
+
+namespace File
 {
+	typedef encoding_t std::string; // Represents an encoding.
 
-	// Type used to deal with file sizes of any weight (GBs are okay).
-	typedef unsigned long filesize_t;
-
-	namespace File
-	{
 //-------------------------------------------------------------- Constants
-		const std::string ENC_UTF16LE = "UTF-16LE";
-		const std::string ENC_UTF8 = "UTF-8";
-		const std::string ENC_DEFAULT = "DEFAULT";
-//------------------------------------------------------------------ Types
+	const encoding_t ENC_UTF16LE = "UTF-16LE";
+	const encoding_t ENC_UTF8 = "UTF-8";
+	const encoding_t ENC_DEFAULT = "DEFAULT";
+	const encoding_t ENC_UNKNOWN = "";
 		
 
 //////////////////////////////////////////////////////////////////  PUBLIC
 //------------------------------------------------------- Public functions
 
-		// Description: Opens given file with the given stream.
-		// Inputs: any ifstreal, any filename.
-		// Warnings: if file cannot be openend for reading, is empty,
-		//	or is ".", throws a FileException.
-		//	If input encoding is not known, opens with UTF-8 (risky).
-		void Open(std::ifstream& ifs, const std::string& fileName);
+	// Returns true if the file exists, false otherwise.
+	// It is the fastest way I know for testing a file existence.
+	bool Exists(const std::string& filename);
 
-		// Description: Returns file size.
-		// Inputs: a valid filename.
-		// Warnings: if anything fails, throws a FileException.
-		filesize_t Size(const std::string& filename);
+	// Returns true if trying to read charsToRead characters from the file fails.
+	bool IsEmpty(const std::string& filename, size_t charsToRead = 0);
+	
+	// Closes ifs and tries to open filename according to Encoding's result.
+	// Applies default locale if encoding is unknown.
+	// To verify success, try ifs.is_open().
+	// If an encoding is given, applies the corresponding locale.
+	void Open(std::ifstream& ifs, const std::string& filename, const encoding_t given = ENC_UNKNOWN);
 
-		// Returns the encoding of the file as one of the strings declared above.
-		// Warnings: If file is empty, throws a FileException.
-		//	If no encoding is found, returns "".
-		std::string Encoding(const std::string& filename);
-	} 
-}
+	// Description: Returns file size, or -1 in case of failure.
+	// Inputs: a valid filename.
+	filesize_t Size(const std::string& filename);
 
+	// Returns the encoding of the file as one of the strings declared above.
+	// It executes "IsEmpty(filename, 3)" firstly and if the result is true
+	// returns "0".
+	// If no encoding is found, returns "".
+	encoding_t Encoding(const std::string& filename);
+} 
 #endif // FILE_H
