34fc5838b781c1b25163fef2802d69a44ae8a2ce
apokkalyps <32598875+apokkalyps@users.noreply.github.com>
2019-07-10 23:56:23 +0200

    Update the Date module

--- C++/Date.cpp
+++ C++/Date.cpp
@@ -5,67 +5,88 @@
 //-------------------------------------------------------- System includes
 
 //------------------------------------------------------ Personal includes
+#include <iomanip>
+#include <sstream>
 #include "Date.h"
 
 //-------------------------------------------------------------- Constants
-int Date::SUPERIOR = 1;
-int Date::INFERIOR = -1;
-int Date::EQUAL = 0;
+const int Date::SUPERIOR = 1;
+const int Date::INFERIOR = -1;
+const int Date::EQUAL = 0;
+const char* Date::pattern = nullptr;
+const MicroSeconds Date::MS_MAX = MicroSeconds(1000000);
+MicroSeconds Date::tolerance = Date::MS_MAX;
+char Date::msSep = NO_MS;
 
 //----------------------------------------------------------------- PUBLIC
 
 //--------------------------------------------------------- Public methods
-int Date::Compare(const Date& d) const 
+int Date::Compare(const Date& d, bool compareMS) const 
 {
-	return EQUAL; 
+	if (*this == d)
+		return EQUAL;
+
+	auto distance = Timedelta(d);
+	// Smtg to consider whether or not look at microseconds
+	// if else
+	return 0;
+} // TODO
+
+double Date::Timedelta(const Date& param) const 
+{
+	return difftime(timet, param.timet) + 
+		double(microseconds - param.microseconds) / double(MS_MAX);
 }
 
-unsigned int Date::Timedelta(const Date&) const 
-{
-	return -1; 
-}
 //-------------------------------------------------- Operator overloadings
-bool Date::operator== (const Date& d) const
+inline bool Date::operator== (const Date& b) const
 {
-	return Compare(d) == EQUAL;
+	if (timet == b.timet)
+		return abs(b.microseconds - microseconds) <= tolerance;
+	else 
+		return false;
 }
 
-bool Date::operator!= (const Date& d) const
+inline bool Date::operator!= (const Date& d) const
 {
-	return Compare(d) != EQUAL;
+	return !(*this == d);
 }
 
-bool Date::operator< (const Date& d) const
+inline bool Date::operator< (const Date& d) const
 {
 	return Compare(d) == INFERIOR;
 }
 
-bool Date::operator> (const Date& d) const
+inline bool Date::operator> (const Date& d) const
 {
 	return Compare(d) == SUPERIOR;
 }
 
-bool Date::operator<= (const Date& d) const
+inline bool Date::operator<= (const Date& d) const
 {
 	return Compare(d) != SUPERIOR;
 }
 
-bool Date::operator>= (const Date& d) const
+inline bool Date::operator>= (const Date& d) const
 {
 	return Compare(d) != INFERIOR;
 }
 
 Date Date::operator+ (const Date& d) const
 {
-	return Date();
+	Date newdate(*this);
+	newdate += d;
+	return newdate;
 }
 
 Date Date::operator- (const Date& d) const
 {
-	return Date();
+	Date newdate(*this);
+	newdate -= d;
+	return newdate;
 }
 
-unsigned int Date::operator% (const Date& d) const
+inline double Date::operator% (const Date& d) const
 {
 	return Timedelta(d);
 }
@@ -73,55 +94,92 @@ unsigned int Date::operator% (const Date& d) const
 Date& Date::operator+= (const Date& d)
 {
 	return *this;
-}
+} // TODO
 
 Date& Date::operator-= (const Date& d)
 {
 	return *this;
-}
+} // TODO
 
-Date::operator struct tm() const
+inline Date::operator struct tm() const
 {
 	return time;
 }
 
-Date::operator time_t () //const
+inline Date::operator time_t () const
 {
-	return mktime(&time);
+	return timet;
 }
 
 Date::operator std::string() const
 {
-	return "";
+	if (pattern == nullptr) 
+		throw DateError::NO_PATTERN;
+
+	std::ostringstream oss;
+	oss << std::put_time(&time, pattern);
+
+	if (msSep != NO_MS)
+		oss << msSep << microseconds;
+
+	return oss.str();
 }
 
 
 //---------------------------------------------- Constructors - destructor
 Date::Date() :
-	time(),
-	microseconds()
+	time({ 0 }),
+	microseconds(0),
+	timet(0)
 {
 }
 
-Date::Date(tm time, unsigned int ms)
+Date::Date(tm time_in, MicroSeconds ms) :
+	microseconds(makeMS(ms)),
+	timet(mktime(&time_in)),
+	time(time_in)
 {
-	if (mktime(&time) == time_t(-1) || ms > 999999)
-		throw nullptr;
+	if (timet == time_t(-1) || ms >= MS_MAX)
+		throw DateError::WRONG_STRUCT_TM;
 
-	this->time = time;
-	microseconds = ms;
+	time = time_in;
 }
 
-Date::Date(time_t tmt, unsigned int ms)
+Date::Date(time_t tmt, MicroSeconds ms) :
+	microseconds(makeMS(ms)),
+	timet(tmt)
 {
-
+	if (localtime_s(&time, &tmt))
+		throw DateError::WRONG_TIME_T;
 }
 
-Date::Date(const std::string&, const char* pattern)
+Date::Date(const std::string& src, const char* pattern, MicroSeconds ms) :
+	microseconds(makeMS(ms))
 {
+	if (pattern == nullptr)
+	{
+		if (Date::pattern != nullptr)
+			pattern = Date::pattern;
+		else
+			throw DateError::NO_PATTERN;
+	}
 
+	std::istringstream iss(src);
+	Date ans;
+	iss >> std::get_time(&time, pattern);
+
+	if (timet = mktime(&time) == time_t(-1)) {
+		throw DateError::WRONG_STRING;
+	}
 }
 	
 //---------------------------------------------------------------- PRIVATE
 
 //------------------------------------------------------ Protected methods
+MicroSeconds Date::makeMS(MicroSeconds ms)
+{
+	if (ms > MS_MAX)
+		throw DateError::WRONG_MS;
+	else
+		return ms;
+}
--- C++/Date.h
+++ C++/Date.h
@@ -10,50 +10,73 @@
 
 //------------------------------------------------------------------ Types
 
+// Type for representing microseconds.
+typedef unsigned short MicroSeconds; 
+
+// Error codes for this module.
+enum DateError
+{
+	NO_PATTERN = 1,
+	WRONG_STRUCT_TM = 2,
+	WRONG_MS = 3,
+	WRONG_STRING = 4,
+	WRONG_TIME_T = 5
+};
+
+// Use this character to specify not to represent microseconds.
+#define NO_MS char(0x00)
+
 // Role of Date: 
 // Simple extension of C's struct tm with milliseconds and operator 
 // overloadings. 
-// Please note that the 'isdst', 'mday', 'wday' fields are ignored.
+// Please note that the 'isdst', 'mday' fields are ignored.
 class Date
 {
 //----------------------------------------------------------------- PUBLIC
 
 public:
 //--------------------------------------------------------- Public methods
-	int Compare(const Date&) const;
-	unsigned int Timedelta(const Date&) const;
+
+	// Compares the current date with the given one.
+	// Returns Dates::INFERIOR if (this) < (param),
+	// same idea for Dates::EQUAL and Dates::SUPERIOR.
+	// The parameter 'compareMS' indicates whether we compare microseconds 
+	// or not.
+	int Compare(const Date&, bool compareMS = false) const;
+
+	// Returns (this) - (param) in seconds.
+	double Timedelta(const Date&) const;
 
 //-------------------------------------------------- Operator overloadings
 
 	// Classic assignment operator.
-	// Inputs: a good-formed Date.
 	Date& operator= (const Date&) = default;
 
-	bool operator== (const Date&) const;
+	// Comparison operators.
+	inline bool operator== (const Date&) const;
+	inline bool operator!= (const Date&) const;
+	inline bool operator< (const Date&) const;
+	inline bool operator> (const Date&) const;
+	inline bool operator<= (const Date&) const;
+	inline bool operator>= (const Date&) const;
 
-	bool operator!= (const Date&) const;
+	// Returns (this) - (param) in seconds.
+	inline double operator% (const Date&) const;
 
-	bool operator< (const Date&) const;
-
-	bool operator> (const Date&) const;
-
-	bool operator<= (const Date&) const;
-
-	bool operator>= (const Date&) const;
-
+	// Add two dates
 	Date operator+ (const Date&) const;
 
 	Date operator- (const Date&) const;
 
-	unsigned int operator% (const Date&) const;
+	
 
 	Date& operator+= (const Date&);
 
 	Date& operator-= (const Date&);
 
-	operator struct tm() const;
+	inline operator struct tm() const;
 
-	operator time_t () ;
+	inline operator time_t () const;
 
 	operator std::string() const;
 
@@ -63,11 +86,11 @@ public:
     // Inputs: a good-formed Date.
 	Date(const Date&) = default;
 
-	Date(tm, unsigned int microseconds = 0);
+	Date(tm, MicroSeconds = 0);
 
-	Date(time_t, unsigned int microseconds = 0);
+	Date(time_t, MicroSeconds = 0);
 
-	Date(const std::string&, const char* pattern = nullptr);
+	Date(const std::string&, const char* pattern = nullptr, MicroSeconds = 0);
 
 	// Default constructor. 
 	Date();
@@ -79,13 +102,35 @@ public:
 
 protected:
 //------------------------------------------------------ Protected methods
-
+	
+	// If value is correct, return it.
+	// Else throw WRONG_MS.
+	static MicroSeconds makeMS(MicroSeconds);
 //--------------------------------------------------- Protected attributes
-	tm time;
-	int microseconds;
-	static bool compareUnderSecond;
-	static const char* pattern;
-	static int INFERIOR, SUPERIOR, EQUAL;
+
+	tm time; // tm struct that holds the current date.
+	time_t timet; // tm struct that holds the current date.
+	
+	// Field that holds microseconds.
+	MicroSeconds microseconds; 
+	
+	// Use it when converting with string fields.
+	// It will be used the way strftime does.
+	static const char* pattern; 
+	
+	// Constants for comparison results.
+	static const int INFERIOR, SUPERIOR, EQUAL; 
+	
+	// Tolerance in microseconds. Initial value of MS_MAX.
+	static MicroSeconds tolerance; 
+	
+	// Max microseconds = 1M.
+	static const MicroSeconds MS_MAX; 
+
+	// Character used to separate the datetime to the microseconds
+	// in the string representations.
+	// NO_MS is default value.
+	static char msSep;
 };
 
 //------------------------------------------------------ Other definitions
