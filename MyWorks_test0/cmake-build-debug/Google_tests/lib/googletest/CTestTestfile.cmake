2e8ddaad88928f75e75374f29a8d202157f3e096
apokkalyps <mfranc.martin@gmail.com>
2019-11-30 01:53:15 +0100

    Moving files

--- MyWorks_test0/cmake-build-debug/Google_tests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cygdrive/c/Users/mfran/CLionProjects/MyWorks/MyWorks_test0/Google_tests/lib/googletest
-# Build directory: /cygdrive/c/Users/mfran/CLionProjects/MyWorks/MyWorks_test0/cmake-build-debug/Google_tests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
