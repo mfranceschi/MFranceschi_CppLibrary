011ee4a7b19e66d9b8a3ee3722fc8f22ff136c0b
apokkalyps <mfranc.martin@gmail.com>
2020-01-24 13:03:10 +0100

    start implementing view optimize sqlite by making it thread unsafe

--- C IrregularVerbs/View.c
+++ C IrregularVerbs/View.c
@@ -2,4 +2,33 @@
 // Created by mfran on 17/01/2020.
 //
 
+#include <ncurses/ncurses.h>
 #include "View.h"
+
+void view_start_up() {
+    initscr();
+    keypad(stdscr, true); /* to be tested */
+}
+
+void show_welcome_screen();
+
+void show_main_menu();
+
+Command view_ask_user_choice();
+
+void show_list_from_letter(char);
+void update_list_from_letter(char, unsigned int nb_rows_scrolled_down);
+void show_research_results(STRING);
+void update_research_results(STRING, unsigned int nb_rows_scrolled_down);
+
+void refresh_screen() {
+    refresh();
+}
+
+void clear_screen() {
+    clear(); /* or erase? */
+}
+
+void view_shut_down() {
+    endwin();
+}
--- C IrregularVerbs/View.h
+++ C IrregularVerbs/View.h
@@ -11,15 +11,52 @@ typedef enum Command_e {
     LIST, SEARCH, EXERCISE, BACK_HOME, QUIT
 } Command;
 
+/**
+ * Starts, allocates and run every view initialization script.
+ */
 void view_start_up();
+
+/**
+ * Shows up a welcome screen without any menu.
+ * May indicate that loading is in progress.
+ */
 void show_welcome_screen();
+
+/**
+ * Shows the program's main menu. The user can do any of the following:
+ * - List verbs by first letter.
+ * - Search verbs by a substring query.
+ * - Do an exercise.
+ * - Quit.
+ * Note: the display is updated but the user does not have control yet.
+ */
 void show_main_menu();
+
+/**
+ * Returns the user's input as a Command.
+ *
+ * @return a Command corresponding to the user's input.
+ */
+Command view_ask_user_choice();
+
 void show_list_from_letter(char);
 void update_list_from_letter(char, unsigned int nb_rows_scrolled_down);
 void show_research_results(STRING);
 void update_research_results(STRING, unsigned int nb_rows_scrolled_down);
+
+/**
+ * If relevant, performs a screen refresh.
+ */
 void refresh_screen();
+
+/**
+ * Cleans the screen (empty or so) but does not perform an explicit refresh.
+ */
 void clear_screen();
+
+/**
+ * Closes and releases any resource associated with the view.
+ */
 void view_shut_down();
 
 #endif //IRREGULARVERBS_VIEW_H
