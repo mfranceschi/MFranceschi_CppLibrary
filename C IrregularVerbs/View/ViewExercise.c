9ccebfb859884673ac0215b3d54c8812ea8ed459
apokkalyps <mfranc.martin@gmail.com>
2020-04-18 23:32:40 +0200

    Remove subprojects that were moved to another repo

--- C IrregularVerbs/View/ViewExercise.c
+++
@@ -1,7 +0,0 @@
-//
-// Created by Martin on 22/02/2020.
-//
-
-#include "View.h"
-#include "ViewInternals.h"
-
--- C++ PrimeNumbers/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Controller.hpp"
-#include <QApplication>
-
-int main(int argc, char** argv) {
-    QApplication app(argc, argv);
-    Controller c;
-    return QApplication::exec();
-}
