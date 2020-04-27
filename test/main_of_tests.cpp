58727798ca05eab94f6c8153f2ece2ef8e25e76b
apokkalyps <mfranc.martin@gmail.com>
2020-04-27 13:17:59 +0200

    failed to detect memory leaks sniff

--- test/main_of_tests.cpp
+++ test/main_of_tests.cpp
@@ -2,30 +2,27 @@
 // Created by mfran on 21/04/2020.
 //
 
-#include <gtest/gtest.h>
-
-// Turn on Memory Leaks detection (Win32 only)
-#ifdef I_Want_Mem_Leaks
-#define _CRTDBG_MAP_ALLOC
-#include <crtdbg.h>
-#endif
+#include "tests_datas.hpp"
 
 // Main function
 int main(int argc, char** argv)
 {
+    ::testing::InitGoogleTest(&argc, argv);
+
 #ifdef I_Want_Mem_Leaks
     _CrtMemState states[3];
-	_CrtMemCheckpoint(&states[0]);
+    _CrtMemCheckpoint(&states[0]);
 #endif
 
-    ::testing::InitGoogleTest(&argc, argv);
     int res = RUN_ALL_TESTS();
 
 #ifdef I_Want_Mem_Leaks
     _CrtMemCheckpoint(&states[1]);
-	if (_CrtMemDifference(&states[2], &states[0], &states[1]))
-		_CrtMemDumpStatistics(&states[2]);
+	if (_CrtMemDifference(&states[2], &states[0], &states[1])) {
+        _CrtMemDumpStatistics(&states[2]);
+    }
 #endif
+
     return res;
 }
 
--- test/tests_datas.hpp
+++ test/tests_datas.hpp
@@ -9,6 +9,13 @@
 #include <MFranceschi_CppLibrary.hpp>
 #include <algorithm>
 
+// Turn on Memory Leaks detection (Win32 only)
+#if defined(_MSC_VER) && defined(I_Want_Mem_Leaks)
+#   define _CRTDBG_MAP_ALLOC
+#   include <cstdlib>
+#   include <crtdbg.h>
+#endif
+
 #define ASSERT_LIST_CONTAINS(container, item) ASSERT_TRUE(std::find(container.cbegin(), container.cend(), item) != container.cend())
 
 #endif //MYWORKS_TEST0_TESTS_DATAS_HPP
