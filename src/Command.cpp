cfd4b7a74066aa0ae02362047cfc6735c1956ea3
apokkalyps <mfranc.martin@gmail.com>
2020-04-29 00:44:12 +0200

    cleanup

--- src/Command.cpp
+++ src/Command.cpp
@@ -9,48 +9,6 @@
 #include "Command.hpp"
 #include "CommandHelper.hpp"
 
-// PRIVATE DECLARATIONS
-
-/**
- * Given a command call, generates the string to pass to the command interpreter.
- * @param commandCall The command call settings
- * @return A string to use.
- */
-static File::SFilename_t prepareCommandString(const CommandCall& commandCall);
-
-// PRIVATE DEFINITIONS
-
-File::SFilename_t prepareCommandString(const CommandCall& commandCall) {
-    File::OSStream_t oss;
-
-    oss << "\"" << commandCall.executable << "\" ";
-
-    for (const auto& argument : commandCall.arguments) {
-        oss << argument << " ";
-    }
-
-    // TODO handle in the good order input output error
-
-    switch (commandCall.inputChoice) {
-        case InputChoice::NONE:
-            break;
-
-        case InputChoice::STRING:
-            oss.seekp(0, std::ios_base::beg);
-            oss << "echo \"" << commandCall.inputString << "\" | ";
-            oss.seekp(0, std::ios_base::end);
-            break;
-        case InputChoice::FROM_FILE: {
-            decltype(oss)::pos_type currentPosition = oss.tellp();
-            oss << " < " << commandCall.inputFile;
-            oss.seekp(currentPosition);
-            break;
-        }
-    }
-
-    return oss.str();
-}
-
 // PUBLIC DEFINITIONS
 
 void Command(const CommandCall& commandCall, CommandReturn& commandReturn) {
@@ -113,4 +71,5 @@ void Command(const CommandCall& commandCall, CommandReturn& commandReturn) {
     if (commandCall.errorsChoice == ErrorChoice::RETRIEVE) {
         commandReturn.errorText = runner.getError();
     }
+    commandReturn.returnCode = runner.getStatusCode();
 }
