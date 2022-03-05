49fbc4b1f24daea9ee2b98f175afb6bd3bf2a5bd
Martin Franceschi <mfranc.martin@gmail.com>
2022-03-05 23:44:49 +0000

    add System module with only DynamicLibrary

---
+++ System/src/Date.cpp
@@ -0,0 +1,357 @@
+//---------- Implementation of class <Date> (file Date.cpp) 
+
+//---------------------------------------------------------------- INCLUDE
+#ifdef nocompil
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include "Date.hpp"
+#include <functional>
+#include <iomanip>
+#include <limits>
+#include <mutex>
+#include "Toolbox.hpp"
+
+//-------------------------------------------------------------- Constants
+
+const char* Date::pattern = nullptr;
+
+#ifdef DATE_MIC_ON
+int Date::tolerance = Date::MS_MAX;
+char Date::msSepChar = NO_MS;
+#endif
+
+#ifdef _WIN32
+#pragma warning(disable: 4127 4309 4365)
+#endif
+constexpr static int MaxYear() noexcept
+
+{
+	using ULL_t = unsigned long long;
+	constexpr ULL_t seconds_in_year(Date::NBR_SECONDS_IN_DAY * 365.2425L);
+	constexpr ULL_t possible_seconds(std::numeric_limits<time_t>::max());
+	constexpr ULL_t max_year_with_time_t(possible_seconds / seconds_in_year);
+	constexpr int max_year_with_struct_tm(std::numeric_limits<int>::max());
+
+	if (max_year_with_time_t > max_year_with_struct_tm)
+		return static_cast<int>(max_year_with_struct_tm);
+	else
+		return static_cast<int>(max_year_with_time_t);
+}
+
+const int Date::MAX_YEAR = MaxYear();
+#ifdef _WIN32
+#pragma warning(default: 4127 4309 4365)
+#endif
+
+Date Date::lastCallToNow = Date::Now();
+
+// Mutexes for static variables access.
+static std::mutex tolerance_mutex, pattern_mutex, msSep_mutex;
+
+// Template function for getter and setter of static variables access.
+template <typename T>
+static T set_static_value(T newValue, std::mutex& mutex_i, T& stored_value, std::function<bool(T)> isReadOnly)
+{
+	mutex_i.lock();
+	T returnValue = isReadOnly(newValue) ? stored_value : (stored_value = newValue);
+	mutex_i.unlock();
+	return returnValue;
+}
+
+#define ASSERT_OK assert((timet = mktime(&time)) != static_cast<time_t>(-1)); /* Ensures the instance is in a valid state or fail assertion. */
+
+//----------------------------------------------------------------- PUBLIC
+
+//--------------------------------------------------------- Public methods
+
+const char* Date::str_pattern(const char* npattern)
+{
+	return set_static_value<decltype(Date::pattern)>(npattern, pattern_mutex, Date::pattern, [](const char* newp) {
+		return newp != nullptr && strlen(newp) == 0;
+		});
+}
+
+int Date::DaysInMonth(int month, int year) noexcept
+{
+	if (month >= 0 && month < 12)
+	{
+		int daysInMonth = 31;
+		if (month == APRIL ||
+			month == JUNE ||
+			month == SEPTEMBER ||
+			month == NOVEMBER
+			)
+			daysInMonth = 30;
+		else if (month == FEBRUARY)
+		{
+			if (IsLeapYear(year))
+				daysInMonth = 29;
+			else
+				daysInMonth = 28;
+		}
+		return daysInMonth;
+	}
+	else return 0;
+}
+
+Date Date::Now() noexcept
+{
+	using namespace std::chrono;
+	using clock = system_clock;
+	clock::time_point now_point = clock::now();
+	Date newDate(clock::to_time_t(now_point));
+#ifdef DATE_MIC_ON
+	using mics = std::chrono::microseconds;
+	mics::rep current_microseconds_count = duration_cast<mics>(now_point.time_since_epoch()).count();
+	newDate.microseconds(static_cast<int>(current_microseconds_count - 1000000 * time_t(newDate)));
+#endif
+	return newDate;
+}
+
+bool Date::Localtime(const time_t& src, struct tm& dest)
+{
+#ifdef _WIN32
+	/* Win32-style Localtime_S is available. */
+	return !localtime_s(&dest, &src);
+
+#elif defined __STDC_LIB_EXT1__ && __STDC_WANT_LIB_EXT1__ == 1
+	/* Classic Localtime_S is available. */
+	return localtime_s(&src, &dest);
+#else
+	/* Use POSIX-style Localtime_R. */
+	return localtime_r(&src, &dest);
+
+	/*
+	 * To use basic "localtime", we should use mutexes to ensure there are no conflicts.
+	 * I am not implementing it here.
+	 */
+#endif
+}
+
+bool Date::Gmtime(const time_t& src, struct tm& dest)
+{
+#ifdef _WIN32
+	/* Win32-style Gmtime_S is available. */
+	return !gmtime_s(&dest, &src);
+
+#elif defined __STDC_LIB_EXT1__ && __STDC_WANT_LIB_EXT1__ == 1
+	/* Classic Gmtime_S is available. */
+	return gmtime_s(&src, &dest);
+#else
+	/* Use POSIX-style Gmtime_R. */
+	return gmtime_r(&src, &dest);
+
+	/*
+	 * To use basic "gmtime", we should use mutexes to ensure there are no conflicts.
+	 * I am not implementing it here.
+	 */
+#endif
+}
+
+long Date::Timezone()
+{
+#ifdef _WIN32
+	long result;
+	_get_timezone(&result);
+	return result;
+#else
+	return timezone;
+#endif
+}
+
+int Date::Compare(const Date& d) const
+{
+#ifdef DATE_MIC_ON
+	return Toolbox::Sign(Timedelta(d));
+#else
+	return Toolbox::Sign(trunc(Timedelta(d)));
+#endif
+}
+
+Interval Date::Timedelta(const Date& param) const
+{
+	Interval result = static_cast<Interval>(difftime(timet, param.timet));
+#ifdef DATE_MIC_ON
+	result += static_cast<Interval>(double(microseconds_in - param.microseconds_in) / double(MS_MAX));
+#endif
+	return result;
+}
+
+// Getters and setters.
+
+int Date::seconds(int newvalue)
+{
+	return quickSetter(newvalue, 0, 60, time.tm_sec);
+}
+
+int Date::minutes(int newvalue)
+{
+	return quickSetter(newvalue, 0, 59, time.tm_min);
+}
+
+int Date::hours(int newvalue)
+{
+	return quickSetter(newvalue, 0, 23, time.tm_hour);
+}
+
+int Date::day_month(int newvalue)
+{
+	if (!newvalue) 
+		newvalue = -2;
+	return quickSetter(newvalue, 1, Date::DaysInMonth(newvalue, time.tm_year - 1900), time.tm_mday);
+}
+
+int Date::month(int newvalue)
+{
+	return quickSetter(newvalue, 0, 11, time.tm_mon);
+}
+
+int Date::year(int newvalue)
+{
+	if (newvalue < MAX_YEAR && newvalue > -MAX_YEAR-1)
+	{
+		time.tm_year = newvalue;
+		ASSERT_OK;
+		return time.tm_year;
+	}
+	else
+		throw DateError::WRONG_TIME_DATA;
+} // TODO
+
+int Date::dst(int newvalue)
+{
+	if (newvalue == DST_UNKNOWN || newvalue == DST_OFF || newvalue == DST_ON)
+	{
+		time.tm_isdst = newvalue;
+		ASSERT_OK;
+		return time.tm_isdst;
+	}
+	else
+		throw DateError::WRONG_TIME_DATA;
+}
+
+#ifdef DATE_MIC_ON
+int Date::microseconds(int newms)
+{
+	if (newms == -1)
+		return microseconds_in;
+	else
+		return microseconds_in = MakeMS(newms);
+}
+
+int Date::ms_tolerance(int newms)
+{
+	return set_static_value<int>(newms, tolerance_mutex, Date::tolerance, [](int ms) -> bool{
+		return ms == -1;
+		});
+}
+
+char Date::ms_CharSep(char newsep)
+{
+	return set_static_value<char>(newsep, msSep_mutex, Date::msSepChar, [](char newc) {
+		return newc == '\r';
+		});
+}
+#endif
+
+//-------------------------------------------------- Operator overloadings
+bool Date::operator== (const Date& b) const {
+#ifdef DATE_MIC_ON
+	using temp_type = long;
+	temp_type current = temp_type(timet) * MS_MAX + microseconds_in;
+	temp_type other = temp_type(b.timet) * MS_MAX + b.microseconds_in;
+	return abs(current - other) <= temp_type(tolerance);
+#else
+	return timet == b.timet;
+#endif
+}
+
+Date::operator std::string() const
+{
+	if (pattern == nullptr) 
+		throw DateError::NO_PATTERN;
+
+	std::ostringstream oss;
+	oss << std::put_time(&time, pattern);
+
+#ifdef DATE_MIC_ON
+	if (msSepChar != NO_MS)
+		oss << msSepChar << microseconds_in;
+#endif
+
+	return oss.str();
+}
+
+
+//---------------------------------------------- Constructors - destructor
+
+Date::Date() :
+	Date(Now())
+{
+}
+
+Date::Date(tm time_in) :
+	time(time_in),
+	timet(mktime(&time))
+{
+	if (timet == time_t(-1))
+		throw DateError::WRONG_STRUCT_TM;
+}
+
+Date::Date(time_t tmt) :
+    time{},
+	timet(tmt)
+{
+	if (!Localtime(timet, time))
+		throw DateError::WRONG_TIME_T;
+}
+
+Date::Date(const std::string& src, const char* npattern) 
+{
+	if (npattern == nullptr)
+	{
+		if (Date::pattern != nullptr)
+			npattern = Date::pattern;
+		else
+			throw DateError::NO_PATTERN;
+	}
+
+	std::istringstream iss(src);
+	Date ans;
+	iss >> std::get_time(&time, npattern);
+
+	if ((timet = mktime(&time)) == time_t(-1)) {
+		throw DateError::WRONG_STRING;
+	}
+}
+
+Date::Date(int year, int month, int monthday, int hour, int minutes, int seconds, int dst_flag)
+{
+	time.tm_year = year;
+	time.tm_mon = month;
+	time.tm_mday = monthday;
+	time.tm_hour = hour;
+	time.tm_min = minutes;
+	time.tm_sec = seconds;
+	time.tm_isdst = dst_flag;
+	ASSERT_OK;
+}
+
+//---------------------------------------------------------------- PRIVATE
+
+//------------------------------------------------------ Protected methods
+
+int Date::quickSetter(int newvalue, int min, int max, int& field)
+{
+	if (newvalue == -1)
+		return field;
+	else
+	{
+		field = Toolbox::Validate(newvalue, DateError::WRONG_TIME_DATA, min, max);
+		ASSERT_OK;
+		return field;
+	}
+}
+#endif // nocompil
---
+++ System/src/DynamicLibrary.cpp
@@ -0,0 +1,108 @@
+//
+// Created by mfran on 04/09/2020.
+//
+
+#include <memory>
+
+#if defined(_WIN32)
+
+#   include <Windows.h>
+#   include "MF/Windows.hpp"
+
+#else
+#   include <dlfcn.h>
+#   include <cstring>
+#   include <signal.h>
+#endif
+
+#include "MF/DynamicLibrary.hpp"
+
+namespace MF {
+    namespace System {
+#if defined(_WIN32)
+        const char *DynamicLibrary::LocalExtension = ".dll";
+#define _GetLibraryPointer static_cast<HMODULE>(_library)
+#else
+        const char* DynamicLibrary::LocalExtension = ".so";
+#endif
+
+        void *DynamicLibrary::GetFunction(const std::string &functionName) {
+            LOCK_t lockGuard(_mutex);
+
+            void *functionAddress;
+
+#if defined(_WIN32)
+            functionAddress = reinterpret_cast<void *>(GetProcAddress(_GetLibraryPointer, functionName.c_str()));
+            if (functionAddress == nullptr) {
+                throw element_not_found_exception("Unable to find given function: " + functionName);
+            }
+#else
+            (void)dlerror(); // Clear any previous error.
+            functionAddress = dlsym(library, functionName.c_str());
+            if (dlerror()) {
+                // We must run this check because the "functionAddress" value may be null but still valid.
+                throw element_not_found_exception ("Unable to find given function: " + functionName);
+            }
+#endif
+            return functionAddress;
+        }
+
+        void DynamicLibrary::AddToSearchPaths(const std::string &path) {
+            if (path.size() < 2) {
+                return;
+            }
+
+#if defined(_WIN32)
+            auto wPath = Windows::ConvertString(path.c_str());
+            AddDllDirectory(wPath.data());
+#else
+            char* ldLibraryPath = getenv("LD_LIBRARY_PATH");
+            char* newLdLibraryPath = (char*) calloc(
+                (ldLibraryPath ? strlen(ldLibraryPath) + 1 : 0) + // Length of the environment variable, if any, + ":" separator
+                path.length() + // Length of the value to add
+                1, // End of string
+                1);
+            strcpy(newLdLibraryPath, ldLibraryPath);
+            strcat(newLdLibraryPath, ":");
+            strcat(newLdLibraryPath, path.c_str());
+            setenv("LD_LIBRARY_PATH", newLdLibraryPath, true);
+#endif
+        }
+
+        DynamicLibrary::~DynamicLibrary() {
+            LOCK_t lockGuard(_mutex);
+
+            bool success;
+#if defined(_WIN32)
+            success = FreeLibrary(_GetLibraryPointer);
+#else
+            success = !dlclose(library);
+#endif
+
+            if (success) {
+                _library = nullptr;
+            } else {
+                // TODO handle error
+            }
+        }
+
+        DynamicLibrary::DynamicLibrary(const std::string &libName) {
+            LOCK_t lockGuard(_mutex);
+
+#if defined(_WIN32)
+            auto wLibName = Windows::ConvertString(libName.c_str());
+            static constexpr HANDLE hFile = nullptr;
+            static constexpr DWORD dwFlags = LOAD_LIBRARY_SEARCH_DEFAULT_DIRS;
+            _library = LoadLibraryExW(wLibName.data(), hFile, dwFlags);
+#else
+            library = dlopen(libName.c_str(), RTLD_LAZY | RTLD_GLOBAL);
+#endif
+
+            if (_library != nullptr) {
+                return; // We can add stuff there
+            } else {
+                throw element_not_found_exception("The requested library could not be found: " + libName);
+            }
+        }
+    }
+}
