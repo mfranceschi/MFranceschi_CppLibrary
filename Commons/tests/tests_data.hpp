452a92b291f86575bcb0ede8f1211bc6c2ce6298
Martin Franceschi <mfranc.martin@gmail.com>
2022-03-05 00:59:54 +0000

    add a working, modular testing infrastructure. remove ugly things

---
+++ Commons/tests/tests_data.hpp
@@ -0,0 +1,20 @@
+//
+// Created by mfran on 02/10/2019.
+//
+
+#ifndef MYWORKS_TEST0_TESTS_DATAS_HPP
+#define MYWORKS_TEST0_TESTS_DATAS_HPP
+
+#include "gtest/gtest.h"
+#include <algorithm>
+
+// Turn on Memory Leaks detection (Win32 only)
+#if defined(_MSC_VER) && defined(I_Want_Mem_Leaks)
+#   define _CRTDBG_MAP_ALLOC
+#   include <cstdlib>
+#   include <crtdbg.h>
+#endif
+
+#define ASSERT_LIST_CONTAINS(container, item) ASSERT_TRUE(std::find(container.cbegin(), container.cend(), item) != container.cend())
+
+#endif //MYWORKS_TEST0_TESTS_DATAS_HPP
