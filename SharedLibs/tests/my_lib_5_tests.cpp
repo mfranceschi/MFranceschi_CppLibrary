070a6e49de407e41101ab6e52bc15a17263e299f
Martin Franceschi <mfranc.martin@gmail.com>
2022-04-18 19:16:58 +0100

    add tests for SharedLibs and add simple features

--- SharedLibs/tests/my_lib_5_tests.cpp
+++ SharedLibs/tests/my_lib_5_tests.cpp
@@ -12,5 +12,9 @@ TEST(SharedLib5Tests, it_throws) {
 
     std::shared_ptr<SharedLib> sharedLib;
 
-    EXPECT_DEATH(sharedLib = OpenExplicitly(MF_SAMPLE_LIB_5_NAME), "Foo");
+    // Currently it abort() on Unix and throws not found on Windows.
+    // TODO: use signal handlers with <csignal> and signal(myFunction, SIGABRT) in Unix
+    // implementations.
+    // TODO: on Windows, ensure a specific exception is thrown and not element_not_found.
+    EXPECT_DEATH(sharedLib = OpenExplicitly(MF_SAMPLE_LIB_5_NAME), "Load function throws");
 }
