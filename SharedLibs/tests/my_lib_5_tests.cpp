4d1cf5844126518e3b09b8b2ecce7545e97937c4
Martin Franceschi <mfranc.martin@gmail.com>
2022-04-23 17:48:24 +0100

    fix Windows build

--- SharedLibs/tests/my_lib_5_tests.cpp
+++ SharedLibs/tests/my_lib_5_tests.cpp
@@ -12,7 +12,8 @@ TEST(SharedLib5Tests, it_handles_exception_during_library_init) {
 
     std::shared_ptr<SharedLib> sharedLib;
 #if MF_WINDOWS
-    EXPECT_THROWS(sharedLib = OpenExplicitly(MF_SAMPLE_LIB_5_NAME), std::runtime_error)
+    EXPECT_THROW(
+        sharedLib = OpenExplicitly(MF_SAMPLE_LIB_5_NAME), SharedLib::element_not_found_exception);
 #else
     EXPECT_DEATH(sharedLib = OpenExplicitly(MF_SAMPLE_LIB_5_NAME), "Load function throws");
 #endif
